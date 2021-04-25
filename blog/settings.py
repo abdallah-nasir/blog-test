6c8e5b229ff84a938e8cbb9cab5cc2d4fe2d1075
abdallah nasir <abdallah.nasir@ymail.com>
2021-04-25 17:05:47 +0200

    commit-7

--- blog/settings.py
+++ blog/settings.py
@@ -244,13 +244,12 @@ SOCIALACCOUNT_PROVIDERS = {
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT= BASE_DIR/"static"
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR/"media"
-
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS=[
-    BASE_DIR/"static_in_env",
+   os.path.join(BASE_DIR, 'static_in_env'),
 ]
 
 
