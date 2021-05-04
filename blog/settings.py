b23c13e966855e5d92d5097172759586fbba7794
Abdallah Nasir <71611014+abdallah-nasir@users.noreply.github.com>
2021-05-04 03:42:24 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["test-homeblog.herokuapp"]
+ALLOWED_HOSTS = ["test-homeblog.herokuapp.com","localhost","127.0.0.1"]
 
 
 # Application definition
