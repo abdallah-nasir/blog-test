d1dcdc3c192678a1769a9cae0d818e6513f5cf43
Abdallah Nasir <71611014+abdallah-nasir@users.noreply.github.com>
2021-05-04 03:44:37 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["test-homeblog.herokuapp.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
